4b7b362296f1f1a6c43c5f1c079f132476dc1536
Kaz Saegusa <chgm.bunbuker@gmail.com>
2019-07-05 14:56:51 +0900

    socket data pipeline from server to the extension

--- ext/background.js
+++ ext/background.js
@@ -1,23 +1,12 @@
 $(window).on('load', () => {
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess);
-  $('body').find('div').each((index, element) => {
-    // console.log(element, index);
-    $(element).css({
-      // transform: 'translate(100px, 100px)'
-    });
+  let socket = io( 'http://localhost:5051' );
+  socket.on('data.update.extension', function(msg) {
+    console.log(msg, 'msg');
   })
+  // $('body').find('div').each((index, element) => {
+    // console.log(element, index);
+    // $(element).css({
+      // transform: 'translate(100px, 100px)'
+    // });
+  // })
 })
-
-var handleSuccess = function(stream) {
-  var context = new AudioContext();
-  var source = context.createMediaStreamSource(stream);
-  var processor = context.createScriptProcessor(1024, 1, 1);
-
-  source.connect(processor);
-  processor.connect(context.destination);
-
-  processor.onaudioprocess = function(e) {
-    // Do something with the data, i.e Convert this to WAV
-    // console.log(e.inputBuffer);
-  };
-};
